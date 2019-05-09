97b80ba14d2bc5bd6330f50ff32c69402200f791
Abhyuday Puri <a.96puri@gmail.com>
2019-05-09 16:15:40 -0400

    data process

--- utils/organize_data.py
+++ utils/organize_data.py
@@ -1,34 +1,45 @@
 import os
 import shutil
+import csv
 
-user = '/home/ecbm6040/dataset5/'
-f= os.listdir(user)
+
+root = '/home/ecbm6040/dataset5/'
+f = os.listdir(root)
 print(f)
 
-# labels = []
-# names =[]
-# train_path = os.path.join('dataset5/','data_A')
-# if not os.path.exists(train_path):
-#     os.makedirs(train_path)
-# count=0
-# for a in f:
-#     path = os.path.join(user,a)
-#     b =  os.listdir(path)
-    
-#     for x in b:
-#         if(x.split('_')[0]=='color'):
-#             labels.append(count)
-#             names.append(x)
 
-#             # print(x)
-#             old_path = os.path.join(path,x)
-#             new_im_path = os.path.join(train_path,x)
-#             #print(old_path,new_im_path)
-#             shutil.move(old_path,new_im_path)
-#     count = count+1
-# cs = [names,labels]
-# import csv
+for person in f:
 
-# with open('dataset5/labels_A.csv','w') as f1:
-#     writer = csv.writer(f1)
-#     writer.writerows(zip(names, labels))
+	labels = []
+	names =[]
+
+	dest_path = os.path.join('/home/ecbm6040', 'dataset', person)
+	if not os.path.exists(dest_path):
+	    os.makedirs(dest_path)
+	
+	src_path = os.path.join(root, person)
+	g = os.listdir(src_path)
+	
+	count=0
+	
+	for letters in g:
+	    path = os.path.join(src_path, letters)
+	    b =  os.listdir(path)
+	    
+	    for x in b:
+	        if(x.split('_')[0]=='color'):
+	            labels.append(count)
+	            names.append(x)
+
+	            # print(x)
+	            old_path = os.path.join(path, x)
+	            new_im_path = os.path.join(dest_path, x)
+	            #print(old_path,new_im_path)
+	            shutil.move(old_path, new_im_path)
+	    count = count+1
+	cs = [names,labels]
+
+	file_name = '/home/ecbm6040/dataset/' + person + '.csv'
+	with open(file_name,'w') as f1:
+	    writer = csv.writer(f1)
+	    writer.writerows(zip(names, labels))
