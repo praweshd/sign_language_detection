b28be03fa4f24439753d55ef411786e300d70057
Abhyuday Puri <a.96puri@gmail.com>
2019-05-09 17:35:30 -0400

    im resize

--- utils/im_resize.py
+++ utils/im_resize.py
@@ -12,8 +12,9 @@ src_dir = os.listdir('/home/ecbm6040/dataset/')
 for im_name in src_dir:
 	template = np.zeros((M, N, 3))
 
+	im_path = os.path.join(src_dir, im_name)
 	# Read the image
-	im = plt.imread(src_dir + im_name)
+	im = plt.imread(im_path)
 
 	# Get shape
 	m, n, _ = im.shape
@@ -22,6 +23,7 @@ for im_name in src_dir:
 	template[np.floor((M-m)/2) : (np.floor((M-m)/2) + m), np.floor((N-n)/2) : (np.floor((N-n)/2) + n), :] = im
 
 	# Save the image
-	plt.imsave('/home/ecbm6040/dataset_final/' + im_name + '.png', template)
+	dst_path = os.path.join('/home/ecbm6040/dataset_final', im_name + '.png')
+	plt.imsave(dst_path, template)
 
 	break
