6c828449cab7b8418f7e8afebd8cc0c3f969f6e8
Abhyuday Puri <a.96puri@gmail.com>
2019-05-09 17:46:56 -0400

    resize image

--- utils/im_resize.py
+++ utils/im_resize.py
@@ -7,23 +7,21 @@ import matplotlib.pyplot as plt
 M = 279
 N = 249
 
-src_dir = os.listdir('/home/ecbm6040/dataset/')
-
+src_dir = os.listdir('/home/ecbm6040/dataset')
 for im_name in src_dir:
-	template = np.zeros((M, N, 3))
+    template = np.zeros((M, N, 3))
+    im_path = os.path.join('/home/ecbm6040/dataset', im_name)
+    
+    # Read the image
+    im = plt.imread(im_path)
+    plt.imshow(im)
+    # Get shape
+    m, n, _ = im.shape
 
-	im_path = os.path.join(src_dir, im_name)
-	# Read the image
-	im = plt.imread(im_path)
+    # Padding the image
+    template[(M-m)//2 : ((M-m)//2 + m), (N-n)//2 : ((N-n)//2 + n), :] = im
+    plt.imshow(template)
 
-	# Get shape
-	m, n, _ = im.shape
-
-	# Padding the image
-	template[np.floor((M-m)/2) : (np.floor((M-m)/2) + m), np.floor((N-n)/2) : (np.floor((N-n)/2) + n), :] = im
-
-	# Save the image
-	dst_path = os.path.join('/home/ecbm6040/dataset_final', im_name + '.png')
-	plt.imsave(dst_path, template)
-
-	break
+    # Save the image
+    dst_path = os.path.join('/home/ecbm6040/dataset_final', im_name)
+    plt.imsave(dst_path, template)
