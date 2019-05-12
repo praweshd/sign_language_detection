e5ce3ebefe8bb0fab6678edffc297f5e6229cabc
Abhyuday Puri <a.96puri@gmail.com>
2019-05-12 15:20:38 -0400

    yo

--- utils/create_new_data.py
+++ utils/create_new_data.py
@@ -3,9 +3,9 @@ import shutil
 import csv
 import pandas as pd 
 
-train = pd.csv_read('/home/ecbm6040/dataset_update/train_1.csv')
-val = pd.csv_read('/home/ecbm6040/dataset_update/val_1.csv')
-test = pd.csv_read('/home/ecbm6040/dataset_update/test_1.csv')
+train = pd.read_csv('/home/ecbm6040/dataset_update/train_1.csv')
+val = pd.read_csv('/home/ecbm6040/dataset_update/val_1.csv')
+test = pd.read_csv('/home/ecbm6040/dataset_update/test_1.csv')
 
 train = train.sample(frac=1)
 print(train)
