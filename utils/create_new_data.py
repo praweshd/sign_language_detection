4d3c8c7b9f871881185a3ecd7e47f53c2d294829
Abhyuday Puri <a.96puri@gmail.com>
2019-05-12 15:43:45 -0400

    update data

--- utils/create_new_data.py
+++ utils/create_new_data.py
@@ -3,16 +3,18 @@ import shutil
 import csv
 import pandas as pd 
 
-train = pd.read_csv('/home/ecbm6040/dataset_update/train_1.csv')
-val = pd.read_csv('/home/ecbm6040/dataset_update/val_1.csv')
-test = pd.read_csv('/home/ecbm6040/dataset_update/test_1.csv')
+train = pd.read_csv('/home/ecbm6040/dataset_update/train1.csv')
+val = pd.read_csv('/home/ecbm6040/dataset_update/val1.csv')
+test = pd.read_csv('/home/ecbm6040/dataset_update/test1.csv')
 
-train = train.sample(frac=1)
-val = val.sample(frac=1)
-test = test.sample(frac=1)
+# train = train.sample(frac=1)
+# val = val.sample(frac=1)
+# test = test.sample(frac=1)
 
-train.to_csv('/home/ecbm6040/dataset_update/train.csv', index = False)
-val.to_csv('/home/ecbm6040/dataset_update/val.csv', index = False)
-test.to_csv('/home/ecbm6040/dataset_update/test.csv', index = False)
+# train.to_csv('/home/ecbm6040/dataset_update/train.csv', index = False)
+# val.to_csv('/home/ecbm6040/dataset_update/val.csv', index = False)
+# test.to_csv('/home/ecbm6040/dataset_update/test.csv', index = False)
 # train.to_csv('/home/ecbm6040/dataset_update/train.csv')
 # train.to_csv('/home/ecbm6040/dataset_update/train.csv')
+
+print(train.label == 24)
