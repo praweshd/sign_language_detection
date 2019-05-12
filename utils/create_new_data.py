e77faaa750fc29520ddfc43c1a29929dfe6cad59
Abhyuday Puri <a.96puri@gmail.com>
2019-05-12 15:23:02 -0400

    yo

--- utils/create_new_data.py
+++ utils/create_new_data.py
@@ -8,4 +8,9 @@ val = pd.read_csv('/home/ecbm6040/dataset_update/val_1.csv')
 test = pd.read_csv('/home/ecbm6040/dataset_update/test_1.csv')
 
 train = train.sample(frac=1)
-print(train)
+val = val.sample(frac=1)
+test = test.sample(frac=1)
+
+train.to_csv('/home/ecbm6040/dataset_update/train.csv', index = False)
+# train.to_csv('/home/ecbm6040/dataset_update/train.csv')
+# train.to_csv('/home/ecbm6040/dataset_update/train.csv')
