0d048ed22f192c48bd11062607ce5feea5d5e567
Mariam Avagyan <mariamavagyan@dyn-129-236-228-115.dyn.columbia.edu>
2019-05-12 02:08:32 -0400

    val

--- train.py
+++ train.py
@@ -95,7 +95,7 @@ for epoch in range(num_epochs):
 			running_loss = 0.0
 			num_correct = 0.0
 
-		if (i % 20 == 0 and i != 0):
+		if (i % 500 == 0 and i != 0):
 			net.eval()
 			val_acc = []
 			 
