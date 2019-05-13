754ebf50b92270cf2ff6bb9d93c59e921f94b7b5
Abhyuday Puri <a.96puri@gmail.com>
2019-05-12 20:00:14 -0400

    weight_decay

--- train.py
+++ train.py
@@ -28,7 +28,7 @@ net.train()
 criterion = nn.CrossEntropyLoss()    # This is the Cross Entropy Loss Function
 
 # Choosing the optimizer and its hyper-parameters
-optimizer = optim.Adam(net.parameters(), lr=1e-4, betas=(0.9, 0.999), eps=1e-08, weight_decay=0.1)    # Adaptive Momentum Optimizer
+optimizer = optim.Adam(net.parameters(), lr=1e-4, betas=(0.9, 0.999), eps=1e-08, weight_decay=0.01)    # Adaptive Momentum Optimizer
 
 # Hyper-Parameters
 num_epochs = 20
